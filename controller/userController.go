10e732771776cc12657fd16b43bc47ccedddaa16
BigRunFaFishKiller <2214016630@qq.com>
2022-08-14 10:49:12 +0800

    用户注册和登录

--- controller/userController.go
+++ controller/userController.go
@@ -5,6 +5,7 @@ import (
 	"gin/model"
 	"gin/util"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 	"log"
 	"net/http"
@@ -44,14 +45,73 @@ func Register(c *gin.Context) {
 		return
 	}
 	//创建用户
+	//对密码进行加密
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"code": 500,
+			"msg":  "服务器错误",
+		})
+		return
+	}
 	newUser := model.User{
 		Name:     name,
-		Password: password,
+		Password: string(hashedPassword),
 		Phone:    phone,
 	}
 	db.Debug().Create(&newUser)
-	c.JSON(200, gin.H{
+	c.JSON(http.StatusOK, gin.H{
+		"code":    200,
 		"message": "注册成功",
+	})
+}
+
+func Login(c *gin.Context) {
+	db := common.GetDB()
+	//获取参数
+	password := c.PostForm("password")
+	phone := c.PostForm("phone")
+	//数据验证
+	if len(phone) != 11 {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code": 422,
+			"msg":  "手机号必须为11位",
+		})
+		return
+	}
+	if len(password) < 6 {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code": 422,
+			"msg":  "密码不能小于6位",
+		})
+		return
+	}
+	var user model.User
+	db.Debug().Where("phone = ?", phone).First(&user)
+	if user.ID == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code": 400,
+			"msg":  "用户不存在",
+		})
+		return
+	}
+	//判断密码是否正确
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code": 400,
+			"msg":  "密码错误",
+		})
+		return
+	}
+	//发放token
+	token := 111
+	c.JSON(http.StatusOK, gin.H{
+		"code": 200,
+		"data": gin.H{
+			"token": token,
+		},
+		"msg": "登录成功",
 	})
 }
 
